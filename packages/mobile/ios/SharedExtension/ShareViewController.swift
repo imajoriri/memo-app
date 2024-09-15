c79313ed11ba8e51a12c8bfa576732b82fb7c3ca
imajoriri <imajo.srroni@gmail.com>
2024-09-15 18:12:03 +0900

    swiftuiで実装

--- packages/mobile/ios/SharedExtension/ShareViewController.swift
+++ packages/mobile/ios/SharedExtension/ShareViewController.swift
@@ -6,23 +6,119 @@
 //
 
 import UIKit
+import SwiftUI
 import Social
 import Flutter
+import UniformTypeIdentifiers
 
 class ShareViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    showFlutter()
+
+    guard
+      let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+      let itemProvider = extensionItem.attachments?.first else {
+      self.close()
+      return
+    }
+    checkUrlType(itemProvider: itemProvider)
+    checkTextType(itemProvider: itemProvider)
+
+    // SwitUIでcloseボタン押した際に、モーダルを閉じる。
+    NotificationCenter.default.addObserver(forName: NSNotification.Name("close"), object: nil, queue: nil) { _ in
+       DispatchQueue.main.async {
+          self.close()
+       }
+    }
   }
 
-  func showFlutter() {
-    let flutterEngine = FlutterEngine(name: "share_extension", project: nil)
-    flutterEngine.run(withEntrypoint: "shareExtension")
-    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+  func checkUrlType(itemProvider: NSItemProvider) {
+    // テキストが共有された場合
+    if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
 
-    addChild(flutterViewController)
-    view.addSubview(flutterViewController.view)
-    flutterViewController.view.frame = view.bounds
+      itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil) { data, error in
+        guard let url = data as? NSURL else { return  }
+        DispatchQueue.main.async {
+          let contentView = UIHostingController(rootView: ShareExtensionView(text: url.absoluteString ?? ""))
+          self.addChild(contentView)
+          self.view.addSubview(contentView.view)
+
+          // set up constraints
+          contentView.view.translatesAutoresizingMaskIntoConstraints = false
+          contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+          contentView.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+          contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+          contentView.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+        }
+      }
+    }
+  }
+
+  func checkTextType(itemProvider: NSItemProvider) {
+    // テキストが共有された場合
+    if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
+      itemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil) { data, error in
+        guard let sharedText = data as? String else { return }
+        DispatchQueue.main.async {
+          let contentView = UIHostingController(rootView: ShareExtensionView(text: sharedText))
+          self.addChild(contentView)
+          self.view.addSubview(contentView.view)
+
+          // set up constraints
+          contentView.view.translatesAutoresizingMaskIntoConstraints = false
+          contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+          contentView.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+          contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+          contentView.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+        }
+      }
+    }
+  }
+
+  func close() {
+    self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+  }
+}
+
+struct ShareExtensionView: View {
+  @State private var text: String
+
+  init(text: String) {
+    self.text = text
+  }
+
+  var body: some View {
+    NavigationStack{
+      VStack(spacing: 20){
+        Text("Text")
+        TextField("Text", text: $text, axis: .vertical)
+          .lineLimit(3...6)
+          .textFieldStyle(.roundedBorder)
+
+        Button {
+          // TODO: Something with the text
+          self.close()
+        } label: {
+          Text("Post")
+            .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
+
+        Spacer()
+      }
+      .padding()
+      .navigationTitle("Share Extension")
+      .toolbar {
+        Button("Cancel") {
+          self.close()
+        }
+      }
+    }
+  }
+
+  // so we can close the whole extension
+  func close() {
+    NotificationCenter.default.post(name: NSNotification.Name("close"), object: nil)
   }
 }
 
