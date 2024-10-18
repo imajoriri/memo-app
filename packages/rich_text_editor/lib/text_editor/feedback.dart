988a41cf4d44ea17c15c1a9a8b4867947b3ac760
imajoriri <imajo.srroni@gmail.com>
2024-10-19 08:45:22 +0900

    fix feedbackk ui

--- packages/rich_text_editor/lib/text_editor/feedback.dart
+++ packages/rich_text_editor/lib/text_editor/feedback.dart
@@ -13,16 +13,33 @@ class _Feedback extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 画面幅より40px小さい
+    final width = MediaQuery.of(context).size.width - 20;
     return Opacity(
       opacity: 0.7,
       child: Material(
-        color: Colors.transparent,
-        child: IntrinsicWidth(
-          child: IntrinsicHeight(
-            child: Provider.value(
-              value: editorState as EditorState,
-              child: blockComponentBuilders[blockComponentContext.node.type]!
-                  .build(blockComponentContext),
+        child: Container(
+          width: width,
+          // shadow
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.16),
+                offset: const Offset(0, 0),
+                blurRadius: 20,
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          child: IntrinsicWidth(
+            child: IntrinsicHeight(
+              child: Provider.value(
+                value: editorState as EditorState,
+                child: blockComponentBuilders[blockComponentContext.node.type]!
+                    .build(blockComponentContext),
+              ),
             ),
           ),
         ),
